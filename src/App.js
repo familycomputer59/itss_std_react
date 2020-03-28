ebd8778c3344025000e2d69e9c4057c3d7c0633f
Tien <viettienha98@gmail.com>
2020-03-28 09:37:27 +0700

    Homework

--- src/App.js
+++ src/App.js
@@ -3,12 +3,69 @@ import ToDoListItem from "./ToDoListItem.js"
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      works: [
+        {
+          title: "Task1",
+          description: "FooBar1"
+        },
+        {
+          title: "Task2",
+          description: "FooBar2"
+        },
+      ],
+    };
+  }
+  mySubmitHandler = (event) => {
+    event.preventDefault();
+    var newArray = this.state.works;
+    newArray.unshift({
+      title: this.refs.task.value,
+      description: this.refs.description.value
+    });
+    this.setState({
+      works: newArray
+    });
+  }
+  triggerDelete(index) {
+    var newArray = this.state.works.filter((_, i) => i !== index);
+    this.setState({
+      works: newArray
+    });
+  }
   render() {
+    console.log(this.state.works);
+    var works = this.state.works.map((item, idx) =>
+      <ToDoListItem
+        title={item.title}
+        description={item.description}
+        onClick={() => this.triggerDelete(idx)}
+      />
+    );
     return (
       <div className="App">
-        <div>
-          <ToDoListItem
+        <form className="App-form" onSubmit={this.mySubmitHandler}>
+          <p>New task</p>
+          <input
+            type="text"
+            name="task"
+            ref="task"
           />
+          <p>Description</p>
+          <input
+            type="description"
+            name="description"
+            ref="description"
+          />
+          <input
+            type='submit'
+            value='Add'
+          />
+        </form>
+        <div>
+          {works}
         </div>
       </div>
     );
--- src/ToDoListItem.js
+++ src/ToDoListItem.js
@@ -3,10 +3,13 @@ import './ToDoListItem.css';
 
 class ToDoListItem extends Component {
   render() {
+    const title = this.props.title;
+    const description = this.props.description;
+    // const index = this.props.index;
     return (
-      <div className="ToDoListItem">
-        <div className="ToDoListItem-title"></div>
-        <div className="ToDoListItem-description"></div>
+      <div className="ToDoListItem" onClick={this.props.onClick}>
+        <div className="ToDoListItem-title">{title}</div>
+        <div className="ToDoListItem-description">{description}</div>
       </div>
     );
   }
