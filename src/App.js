f332fa2a27e4dff1c1274c1e8ddae784749e1837
huyen-tt <thanhhuyentran1998@gmail.com>
2020-03-28 09:14:36 +0000

    ToDo App

--- src/App.js
+++ src/App.js
@@ -1,14 +1,52 @@
 import React, { Component } from 'react';
 import ToDoListItem from "./ToDoListItem.js"
 import './App.css';
+import ToDoForm from "./ToDoForm";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      toDoLists: [
+        {
+          title: "title1",
+          description: "description1"
+        },
+        {
+          title: "title2",
+          description: "description2"
+        },
+      ]
+    };
+    this.handleDeleteToDo = this.handleDeleteToDo.bind(this);
+  }
+  handleAddToDo = item => {
+    this.state.toDoLists.push(item);
+    this.setState({ toDoLists: this.state.toDoLists });
+  };
+  handleDeleteToDo = index => {
+    this.state.toDoLists.splice(index, 1);
+    this.setState({ toDoLists: this.state.toDoLists });
+  };
+  
   render() {
     return (
+      <div>
       <div className="App">
+        <h1 className="mb-4">ToDo App</h1>
+        <ToDoForm onAddToDo={this.handleAddToDo} />
         <div>
-          <ToDoListItem
-          />
+          {this.state.toDoLists.map((item, index) => {
+              return (
+                <ToDoListItem
+                  key={index}
+                  title={item.title}
+                  description={item.description}
+                  onToDoDelete={() => this.handleDeleteToDo(index)}
+                />
+              );
+            })}
+          </div>
         </div>
       </div>
     );
