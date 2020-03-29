d72dfaed6b823d8b998d2e8f0a82e5de52bd5ce0
tuandang98 <49324133+tuandang98@users.noreply.github.com>
2020-03-30 02:18:23 +0700

    Update ToDoListItem.js

--- src/ToDoListItem.js
+++ src/ToDoListItem.js
@@ -2,11 +2,22 @@ import React, { Component } from 'react';
 import './ToDoListItem.css';
 
 class ToDoListItem extends Component {
+  removeItem=()=>{
+    var i=this.props.id;
+    var removeItem=this.props.removeItem;
+    removeItem(i);
+  }
   render() {
+    const {
+      title,
+      description,
+      id,
+      removeItem
+    } = this.props;
     return (
-      <div className="ToDoListItem">
-        <div className="ToDoListItem-title"></div>
-        <div className="ToDoListItem-description"></div>
+      <div className="ToDoListItem"  onClick={this.removeItem}>
+        <div className="ToDoListItem-title"  >{this.props.title}</div>
+        <div className="ToDoListItem-description" >{this.props.description}</div>
       </div>
     );
   }
