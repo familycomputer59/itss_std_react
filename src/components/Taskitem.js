969ec48bfa20b5f276adf3b627b9a0d468c55f7b
hungking98 <hungking98@gmail.com>
2020-03-29 21:20:00 +0700

    TodoApp-20166222

---
+++ src/components/Taskitem.js
@@ -0,0 +1,31 @@
+import React from "react";
+
+class Taskitem extends React.Component {
+  
+  onDelete = ()=>{
+    this.props.onDelete(this.props.index);
+    
+  }
+  render() {
+  var task = this.props.task;
+  var index = this.props.index;
+    return (
+        <tr>
+        <td>{index}</td>
+        <td>{task.title}</td>
+        <td>{task.mieuta}</td>
+        <td>
+        <button 
+        type="button" 
+        className="btn btn-danger"
+        onClick = {this.onDelete}
+        >Delete Task</button>
+        </td>
+      </tr>
+    );
+  }
+}
+
+export default Taskitem;
+
+
