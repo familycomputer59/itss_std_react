3b41bd0ab457db0a81ccad6d42e8250c19c7e339
グエン・ドゥック・ドー <do.nd167131@sis.hust.edu.vn>
2020-03-29 18:03:10 +0000

    ToDo App

--- src/App.js
+++ src/App.js
@@ -3,12 +3,41 @@ import ToDoListItem from "./ToDoListItem.js"
 import './App.css';
 
 class App extends Component {
+  
+  constructor(){
+      super();
+      this.state = {todoList: Array()};
+  }
+  
+  addItem(event){
+      event.preventDefault();
+      var title = event.target[0];
+      var description = event.target[1];
+      this.setState({todoList: this.state.todoList.concat({title: title.value, description: description.value})});
+      title.value = "";
+      description.value = "";
+  }
+  
+  deleteItem(event, index){
+      this.setState({todoList: this.state.todoList.filter(function(item,i){
+          return i!=index;
+      })});
+  }
+    
   render() {
     return (
       <div className="App">
+        <form className="App-form" method="post" onSubmit={(event)=>this.addItem(event)}>
+            <input type="text" name="title" placeholder="title"/>
+            <textarea name="description" rows="5" placeholder="description"/>
+            <button type="submit">Submit</button>
+        </form>
         <div>
-          <ToDoListItem
-          />
+            {
+                this.state.todoList.map(function(item, index){
+                   return <div onClick={(event)=>this.deleteItem(event, index)}><ToDoListItem id={index} title={item.title} description={item.description} /></div>;
+                },this)
+            }
         </div>
       </div>
     );
