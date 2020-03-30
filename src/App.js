72ffc8faf271260be88528be15de4d54f7257f9b
phamchien98 <phamchien22598hn@gmail.com>
2020-03-30 16:06:06 +0700

    ToDoApp

--- src/App.js
+++ src/App.js
@@ -2,17 +2,47 @@ import React, { Component } from 'react';
 import ToDoListItem from "./ToDoListItem.js"
 import './App.css';
 
+import FormApp from './FormApp.js'
+
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div>
-          <ToDoListItem
-          />
-        </div>
-      </div>
-    );
-  }
+    constructor() {
+        super();
+        this.state = {
+            todos: []
+        }
+    }
+    
+    handleAdd = (todo) => {
+        this.state.todos.push(todo)
+        this.setState({ todos: this.state.todos })
+    }
+    
+    handleDelete = (index) => {
+        this.state.todos.splice(index, 1)
+        this.setState({ todos: this.state.todos })
+    }
+    
+    render() {
+        var todoItems = this.state.todos.map((todo, key) => (
+            <ToDoListItem
+                key={key}
+                title={todo.title}
+                description={todo.description}
+                onDelete = { () => this.handleDelete(key) }
+            />
+        ))
+        
+        
+        return (
+          <div className="App">
+            <h1> Todo App </h1>
+            < FormApp onAdd = { this.handleAdd } />
+            <div>
+            { todoItems }
+            </div>
+          </div>
+        );
+    }
 }
 
 export default App;
