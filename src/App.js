ee3907c1b771f25614f79afedc44ddde23dee064
dinhson2905 <dinhson2905@gmail.com>
2020-03-30 17:11:47 +0700

    done

--- src/App.js
+++ src/App.js
@@ -1,14 +1,43 @@
 import React, { Component } from 'react';
 import ToDoListItem from "./ToDoListItem.js"
+import FormApp from './FormApp.js'
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      todos: []
+    }
+  }
+
+  handleAdd = (todo) => {
+    this.state.todos.push(todo)
+    this.setState({ todos: this.state.todos })
+  }
+
+  handleDelete = (index) => {
+    this.state.todos.splice(index, 1)
+    this.setState({ todos: this.state.todos })
+  }
+
   render() {
+    var todoItems = this.state.todos.map((todo, key) => (
+      <ToDoListItem
+        key={key}
+        title={todo.title}
+        description={todo.description}
+        onDelete={() => this.handleDelete(key)}
+      />
+    ))
+
+
     return (
       <div className="App">
+        <h1> Todo App </h1>
+        <FormApp onAdd={this.handleAdd} />
         <div>
-          <ToDoListItem
-          />
+          {todoItems}
         </div>
       </div>
     );
---
+++ src/FormApp.js
@@ -0,0 +1,59 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class FormApp extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            title: '',
+            description: ''
+        };
+        this.handleChangeTitle = this.handleChangeTitle.bind(this);
+        this.handleChangeDescription = this.handleChangeDescription.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleChangeTitle(event) {
+        this.setState({ title: event.target.value })
+    }
+
+    handleChangeDescription(event) {
+        this.setState({ description: event.target.value })
+    }
+
+    handleSubmit(event) {
+        // alert(this.state.title)
+        this.props.onAdd(this.state)
+        this.setState({
+            title: '',
+            description: ''
+        })
+        event.preventDefault()
+    }
+
+    render() {
+        return (
+            <div>
+                <form className="App-form">
+                    <input
+                        type="text"
+                        name="title"
+                        placeholder="Title"
+                        value={this.state.title}
+                        onChange={this.handleChangeTitle}
+                    />
+                    <textarea
+                        type="text"
+                        name="description"
+                        placeholder="Description"
+                        value={this.state.description}
+                        onChange={this.handleChangeDescription}
+                    />
+                    <button className="button" onClick={this.handleSubmit}>Add</button>
+                </form>
+            </div>
+        );
+    }
+}
+
+export default FormApp;
