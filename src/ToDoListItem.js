6ae89f59e1c592da181ca0c48fb027bacc4c7f35
Le Ba Thien <38976540+boummm@users.noreply.github.com>
2020-03-31 22:33:49 +0700

    finish

--- src/ToDoListItem.js
+++ src/ToDoListItem.js
@@ -3,13 +3,16 @@ import './ToDoListItem.css';
 
 class ToDoListItem extends Component {
   render() {
-    return (
-      <div className="ToDoListItem">
-        <div className="ToDoListItem-title"></div>
-        <div className="ToDoListItem-description"></div>
-      </div>
-    );
-  }
+    if (this.props.list) {
+        return this.props.list.map((item, index) => (
+                <div className="ToDoListItem" onClick={() =>{this.props.deleteItem(index)}}>
+                    <div className="ToDoListItem-title">{item.title}</div>
+                    <div className="ToDoListItem-description">{item.description}</div>
+                </div>
+            )
+        );
+    }
+}
 }
 
 export default ToDoListItem;
