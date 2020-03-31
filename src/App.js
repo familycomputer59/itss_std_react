ec87379f026c26b89a85d4667394bf11c3be0ecb
tonytit98 <tonytit15598@gmail.com>
2020-03-31 15:08:52 +0000

    Finish

--- src/App.js
+++ src/App.js
@@ -3,16 +3,66 @@ import ToDoListItem from "./ToDoListItem.js"
 import './App.css';
 
 class App extends Component {
+    constructor(props) {
+    super(props);
+    this.state = {
+      todolist: [],
+      title: '',
+      description: ''
+    };
+    this.handleTitle = this.handleTitle.bind(this);
+    this.handleDescription = this.handleDescription.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleTitle(event) {
+    this.setState({ title: event.target.value });
+  }
+  handleDescription(event) {
+    this.setState({ description: event.target.value });
+  }
+  handleSubmit(event) {
+    event.preventDefault();
+    let templist = this.state.todolist;
+    templist.push({
+      title: this.state.title,
+      description: this.state.description
+    });
+    this.setState({
+      todolist: templist,
+      title: '',
+      description: ''
+    });
+  }
+
   render() {
-    return (
+      var todolist = this.state.todolist.map((item, id) => (
+      <ToDoListItem
+        id={id}
+        title={item.title}
+        description={item.description}
+      />
+    ));
+      return (
       <div className="App">
+        <form className="App-form">
+          <input 
+            placeholder="Title"
+            value={this.state.title} 
+            onChange={this.handleTitle} 
+            />
+          <textarea
+            placeholder="Description"
+            value={this.state.description} 
+            onChange={this.handleDescription} 
+            />
+          <button onClick={this.handleSubmit}>登録</button>
+        </form>
         <div>
-          <ToDoListItem
-          />
+            {todolist}
         </div>
       </div>
     );
   }
 }
-
 export default App;
--- src/ToDoListItem.js
+++ src/ToDoListItem.js
@@ -3,10 +3,15 @@ import './ToDoListItem.css';
 
 class ToDoListItem extends Component {
   render() {
+      const{
+          title,
+          description
+      } = this.props;
+      //Title description
     return (
       <div className="ToDoListItem">
-        <div className="ToDoListItem-title"></div>
-        <div className="ToDoListItem-description"></div>
+        <div className="ToDoListItem-title">{this.props.title}</div>
+        <div className="ToDoListItem-description">{this.props.description}</div>
       </div>
     );
   }
