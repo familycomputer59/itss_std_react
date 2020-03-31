ca3db90b6da55a952756d52c9a18d74d9fa8acad
XuanLanhbk <55599200+XuanLanhbk@users.noreply.github.com>
2020-03-31 14:09:43 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,6 +3,18 @@ import ToDoListItem from "./ToDoListItem.js"
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      todolist: [],
+      title: '',
+      description: ''
+    };
+    this.handleTitle = this.handleTitle.bind(this);
+    this.handleDescription = this.handleDescription.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleDelete = this.handleDelete.bind(this);
+  }
   render() {
     return (
       <div className="App">
