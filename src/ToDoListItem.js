ac61e4a920ede6855d3b06fae7b5c9b2690afce0
poipoi0099 <56574097+poipoi0099@users.noreply.github.com>
2020-03-31 21:22:19 +0700

    Update ToDoListItem.js

--- src/ToDoListItem.js
+++ src/ToDoListItem.js
@@ -1,14 +1,84 @@
-import React, { Component } from 'react';
-import './ToDoListItem.css';
-
 class ToDoListItem extends Component {
+  state = {
+    todos: [
+      {id: 1, content: 'eat'},
+      {id: 2, content: 'sleep'},
+      {id: 3, content: 'play'},
+      {id: 4, content: 'repeat'}
+    ]
+  }
+  deleteTodo = (id) => {
+    const todos = this.state.todos.filter(todo => {
+      return todo.id !== id
+    });
+    this.setState({
+      todos
+    });
+  }
+  addTodo = (todo) => {
+    todo.id = Math.random();
+    let todos = [...this.state.todos, todo];
+    this.setState({
+      todos
+    });
+  }
   render() {
     return (
       <div className="ToDoListItem">
-        <div className="ToDoListItem-title"></div>
+        <div className="ToDoListItem-title">To Do List</div>
         <div className="ToDoListItem-description"></div>
+        <Todos todos={this.state.todos} deleteTodo={this.deleteTodo} />
+        <AddTodo addTodo={this.addTodo} />
       </div>
     );
+  }
+}
+const Todos = ({todos, deleteTodo}) => {
+
+  const todoList = todos.length ? (
+    todos.map(todo => {
+      return (
+        <div className="collection-item" key={todo.id}>
+          <span onClick={() => {deleteTodo(todo.id)}}>{todo.content}</span>
+        </div>
+      )
+    })
+  ) : (
+    <p className="center">You have no todo left!</p>
+  );
+
+  return (
+    <div className="todos collection">
+      {todoList}
+    </div>
+  )
+}
+class AddTodo extends Component {
+  state = {
+    content: ''
+  }
+  handleChange = (e) => {
+    this.setState({
+      content: e.target.value
+    });
+  }
+  handleSubmit = (e) => {
+    e.preventDefault();
+    // call function to add a todo
+    this.props.addTodo(this.state);
+    this.setState({
+      content: ''
+    })
+  }
+  render() {
+    return (
+      <div>
+        <form onSubmit={this.handleSubmit}>
+          <label>Add a new todo:</label>
+          <input type="text" onChange={this.handleChange} value={this.state.content} />
+        </form>
+      </div>
+    )
   }
 }
 
