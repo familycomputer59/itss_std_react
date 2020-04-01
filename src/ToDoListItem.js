2223d7e201b42a5b21f10542f2067a317121786a
XuanLanhbk <55599200+XuanLanhbk@users.noreply.github.com>
2020-04-01 09:35:16 +0700

    Update ToDoListItem.js

--- src/ToDoListItem.js
+++ src/ToDoListItem.js
@@ -2,14 +2,19 @@ import React, { Component } from 'react';
 import './ToDoListItem.css';
 
 class ToDoListItem extends Component {
+  handleDeleteItem = () => {
+    this.props.onToDoDelete();
+  };
   render() {
-    return (
-      <div className="ToDoListItem">
-        <div className="ToDoListItem-title"></div>
-        <div className="ToDoListItem-description"></div>
-      </div>
-    );
-  }
+    if (this.props.list) {
+	    return this.props.list.map((item, index) => (
+	    <div className="ToDoListItem" onClick={() =>{this.props.deleteHandler(index)}}>
+	      <div className="ToDoListItem-title">{item.title}</div>
+	      <div className="ToDoListItem-description">{item.description}</div>
+	    </div>
+	  );
+	  }
+	}
 }
 
 export default ToDoListItem;
