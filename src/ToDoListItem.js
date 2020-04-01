37127c8243f97a3a5d6bb8be2d853c98ffb3dfc6
thuyquynhcao <caoquynh2@gmail.com>
2020-04-01 02:37:34 +0000

    updat

--- src/ToDoListItem.js
+++ src/ToDoListItem.js
@@ -3,13 +3,14 @@ import './ToDoListItem.css';
 
 class ToDoListItem extends Component {
   render() {
+    const {title, description, onItemClick} = this.props;
     return (
-      <div className="ToDoListItem">
-        <div className="ToDoListItem-title"></div>
-        <div className="ToDoListItem-description"></div>
+      <div className="ToDoListItem" onClick={onItemClick}>
+        <div className="ToDoListItem-title">{title}</div>
+        <div className="ToDoListItem-description">{description}</div>
       </div>
     );
   }
-}
+} 
 
 export default ToDoListItem;
