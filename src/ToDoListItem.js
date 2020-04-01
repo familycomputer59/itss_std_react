c469743cd7e2a2627b257055020d3f9536312512
Nguyễn Đức Anh <38862063+ducMNSD@users.noreply.github.com>
2020-04-01 16:48:59 +0700

    Update ToDoListItem.js

--- src/ToDoListItem.js
+++ src/ToDoListItem.js
@@ -4,12 +4,17 @@ import './ToDoListItem.css';
 class ToDoListItem extends Component {
   render() {
     return (
-      <div className="ToDoListItem">
-        <div className="ToDoListItem-title"></div>
-        <div className="ToDoListItem-description"></div>
+      <div className="ToDoListItem" onClick ={this.props.onClick}>
+        <div className="ToDoListItem-title">{this.props.title}</div>
+        <div className="ToDoListItem-description">{this.props.description}</div>
       </div>
     );
   }
 }
+
+ToDoListItem.defaultProps = {
+  title: "React(1)",
+  description : "課題１の提出"
+};
 
 export default ToDoListItem;
