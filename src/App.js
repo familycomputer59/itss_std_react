176eb22890b9755df4d8c4daa4d9ba364436e69a
Duong Le <tduongle10@gmail.com>
2020-04-01 15:33:52 +0700

    show todos

--- src/App.js
+++ src/App.js
@@ -3,12 +3,30 @@ import ToDoListItem from "./ToDoListItem.js"
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      todos: [{
+        title: 'Hello',
+        description: 'World'
+      }]
+    }
+  }
+
   render() {
     return (
       <div className="App">
         <div>
-          <ToDoListItem
-          />
+          {
+            this.state.todos[0] ?
+              this.state.todos.map((todo) =>
+                <ToDoListItem
+                  title={todo.title}
+                  description={todo.description}
+                />
+              ) :
+              <div>ない</div>
+          }
         </div>
       </div>
     );
