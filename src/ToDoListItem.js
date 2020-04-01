8d8c7ff3425a7114aa4f2d41a91028861cf91f92
huy <huytientai@gmail.com>
2020-04-01 01:51:39 +0000

    abcc

--- src/ToDoListItem.js
+++ src/ToDoListItem.js
@@ -2,14 +2,48 @@ import React, { Component } from 'react';
 import './ToDoListItem.css';
 
 class ToDoListItem extends Component {
+    constructor(){
+        super();
+        this.state = {
+            val : "",
+            list : [],
+        }
+        this.updateVal = this.updateVal.bind(this)
+    }
+    
+    add(){
+        this.setState({list: this.state.list.concat(this.state.val)})
+    }
+    
+    updateVal(event){
+        this.setState({val:event.target.value})
+    }
+    
   render() {
     return (
-      <div className="ToDoListItem">
-        <div className="ToDoListItem-title"></div>
-        <div className="ToDoListItem-description"></div>
-      </div>
+        <div className="ToDoListItem">
+            <div className="ToDoListItem-title">
+                <h3>To Do</h3>
+            </div>
+            <div className="ToDoListItem-description">
+                <div>
+                    <input onChange={this.updateVal}/>
+                </div>
+                <div>
+                    <button onClick={()=>this.add()}>Add</button>
+                    <ul>{this.state.list.map(item => (
+                        <li key={item}>{item}</li>
+                        ))}
+                    </ul>
+                    
+                </div>
+
+            </div>
+        </div>
+      
     );
   }
+ 
 }
 
 export default ToDoListItem;
