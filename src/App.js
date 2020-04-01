99e1f03a680fc0d2a178d9ecf39a0f484f1ce640
Nguyen Dinh Minh <minh.ngdi@gmail.com>
2020-04-01 08:57:31 +0700

    delete files

--- src/App.js
+++ src/App.js
@@ -1,18 +1,19 @@
 import React, { Component } from 'react';
-import ToDoListItem from "./ToDoListItem.js"
-import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div>
-          <ToDoListItem
-          />
-        </div>
-      </div>
-    );
-  }
+    render() {
+        return ( <
+            div className = "App" >
+            <
+            div >
+            <
+            ToDoListItem /
+            >
+            <
+            /div> <
+            /div>
+        );
+    }
 }
 
 export default App;
