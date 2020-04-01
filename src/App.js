7c395fdbad227c298f90c4a4d9cec3b547f4a1a1
Nhocden <47529817+Nhocden@users.noreply.github.com>
2020-04-01 16:08:29 +0700

    add App.js

--- src/App.js
+++ src/App.js
@@ -3,12 +3,59 @@ import ToDoListItem from "./ToDoListItem.js"
 import './App.css';
 
 class App extends Component {
+    constructor(){
+    super();
+    this.state = {
+      List : [
+        {
+          title: "中間テスト",
+          description: "4/12  中間テスト"
+        },
+        {
+          title: "Thin先生からの依頼（１）",
+          description: "テストの作成"
+        },
+        {
+          title: "Thin先生からの依頼（２）",
+          description: "スライドの作成"
+        }
+      ]
+    }
+    this.deleteitems = this.deleteitems.bind(this);
+    this.additems = this.additems.bind(this);
+  }
+
+  additems = (item) => {
+    this.state.List.push(item);
+    this.setState({ List: this.state.List });
+  };
+
+  deleteitems = index => {
+    this.state.List.splice(index, 1);
+    this.setState({ List: this.state.List });
+  };
+
   render() {
+    let items = this.state.List.map((item, index) => {
+      let result = ""
+      if (item) {
+        result = <ToDoListItem 
+                  key={index}
+                  title={item.title}
+                  description={item.description}
+                  Click={() => this.deleteitems(index)}
+                />
+      }
+      return result
+    })
+
     return (
       <div className="App">
+        <div className="App-form">
+          <Form onadd={this.additems}/>
+        </div>
         <div>
-          <ToDoListItem
-          />
+          {items}
         </div>
       </div>
     );
