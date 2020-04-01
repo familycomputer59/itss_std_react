9c8ed69a88c0aac42066fe41ef6e481ef993897d
Duong Le <62134845+duong-le@users.noreply.github.com>
2020-04-01 16:52:26 +0700

    Update ToDoListItem.js

--- src/ToDoListItem.js
+++ src/ToDoListItem.js
@@ -5,8 +5,10 @@ class ToDoListItem extends Component {
   render() {
     return (
       <div className="ToDoListItem">
-        <div className="ToDoListItem-title"></div>
-        <div className="ToDoListItem-description"></div>
+        <div className="ToDoListItem-title">{this.props.item.title}</div>
+        <div className="ToDoListItem-description">
+          {this.props.item.content}
+        </div>
       </div>
     );
   }
