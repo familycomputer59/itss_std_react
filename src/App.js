509ffb763e83493d57ca2dd7241c328c65f11ccf
baohoangbk235 <41557830+baohoangbk235@users.noreply.github.com>
2020-04-01 16:28:50 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,12 +3,72 @@ import ToDoListItem from "./ToDoListItem.js"
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      toDoList: [
+        {
+            title: "テスト",
+            description: "4/12  中間テスト"
+        },
+        {
+            title: "Thin先生からの依頼",
+            description: "テストの作成"
+        },
+      ],
+      item: {
+        title: '',
+        description: '',
+      },
+    }
+  }
+
+  handleTitleEdit(e) {
+    this.state.item.title = e.target.value;
+    this.setState({ item: this.state.item });
+  }
+
+  handleDesEdit(e) {
+    this.state.item.description = e.target.value;
+    this.setState({ item: this.state.item });
+  }
+
+  handleDeleteItem(index) {
+    this.state.toDoList.splice(index, 1);
+    this.setState({ toDoList: this.state.toDoList });
+  }
+
+  handleAddItem() {
+    //this.state.toDoList.push(this.state.item);
+    this.setState({ toDoList: [...this.state.toDoList, this.state.item] }, () => {
+        localStorage.setItem("toDoList", JSON.stringify(this.state.toDoList))
+    });
+  }
+
+  handleSubmit(e) {
+    this.handleAddItem();
+    e.preventDefault();
+    this.setState({
+        item: {
+            title: '',
+            description: ''
+        }
+    })
+  }
+
   render() {
     return (
       <div className="App">
         <div>
-          <ToDoListItem
+          <ToDoListItem entries={this.state.toDoList} 
           />
+        </div>
+        <div>
+          <form onSubmit={(e) => this.handleSubmit(e)}>
+            <input name="title" type="text" value={this.state.item.title} onChange={(e)=>this.handleTitleEdit(e)}  placeholder="Title"/><br />
+            <input name="title" type="text" value={this.state.item.description} onChange={(e)=>this.handleDesEdit(e)} placeholder="Description"/><br />
+            <button type="submit">登録</button>
+          </form>
         </div>
       </div>
     );
