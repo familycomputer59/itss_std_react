a1bf99b209b4859a594fc3d092ab434e10a1fe20
Khanhly30 <53520159+khanhlyvu30@users.noreply.github.com>
2020-04-01 16:58:04 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,51 @@
 import React, { Component } from 'react';
-import ToDoListItem from "./ToDoListItem.js"
 import './App.css';
+import TodoListItem from './TodoListItem';
+import TodoListInput from './TodoListInput';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      items: [
+        { title: 'Learn', content: 'Learn English', isDone: true },
+        { title: 'Eat', content: 'Eat banh my', isDone: false },
+        { title: 'Sleep', content: 'Sleep 8 hours', isDone: true }
+      ]
+    };
+  }
+
+  onChange(index) {
+    const newItems = this.state.items.slice();
+    newItems[index].isDone = !newItems[index].isDone;
+    this.setState({
+      items: newItems
+    });
+    console.log(this.state.items[index]);
+  }
+
+  onRemove(index) {
+    const newItems = this.state.items.slice();
+    newItems.splice(index, 1);
+    this.setState({ items: newItems });
+  }
+
+  onSubmit() {
+    console.log('hi');
+  }
   render() {
     return (
       <div className="App">
-        <div>
-          <ToDoListItem
-          />
+        <div className="item-group">
+          {this.state.items.map((element, index) => (
+            <TodoListItem
+              key={index}
+              item={element}
+              onChange={() => this.onChange(index)}
+              onRemove={() => this.onRemove(index)}
+            />
+          ))}
+          <TodoListInput onSubmit={() => this.onSubmit()} />
         </div>
       </div>
     );
