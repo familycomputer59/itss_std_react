fa4bc786f7c39b9d435240c3ae67112a06a0116a
nguyennd-0051 <nguyen.duy.nguyen@sun-asterisk.com>
2020-04-01 11:43:24 +0700

    Nguyen Duy Nguyen 20162989 木１

--- src/ToDoListItem.js
+++ src/ToDoListItem.js
@@ -2,11 +2,17 @@ import React, { Component } from 'react';
 import './ToDoListItem.css';
 
 class ToDoListItem extends Component {
+  
   render() {
+    const {
+      element,
+      ...props
+    } = this.props;
+
     return (
-      <div className="ToDoListItem">
-        <div className="ToDoListItem-title"></div>
-        <div className="ToDoListItem-description"></div>
+      <div className="ToDoListItem"{...props}>
+        <div className="ToDoListItem-title">{this.props.element.title}</div>
+        <div className="ToDoListItem-description">{this.props.element.description}</div>
       </div>
     );
   }
