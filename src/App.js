23a8c079ae6da1426732611c6acc4435367248f1
LeVoThuUyen <uyenlvt31@gmail.com>
2020-04-01 08:57:05 +0700

    to do app

--- src/App.js
+++ src/App.js
@@ -3,12 +3,85 @@ import ToDoListItem from "./ToDoListItem.js"
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      inputTitle: '',
+      inputDescription: '',
+      todoList: []
+    };
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    const {todoList,inputTitle,inputDescription} = this.state;
+    const newData = todoList.concat([{
+      title: inputTitle,
+      description: inputDescription,
+    }]);
+    this.setState({
+      todoList: newData,
+      inputTitle: '',
+      inputDescription: ''
+    })
+  }
+
+  deleteTodo = (value) => {
+    const {todoList} = this.state;
+    const newData = todoList.filter((element) => element!==value);
+    this.setState({todoList: newData});
+  }
+
+  handleChangeDescription = (e) => {
+    this.setState({
+      inputDescription: e.target.value
+    })
+  }
+
+  handleChangeTitle = (e) => {
+    this.setState({
+      inputTitle: e.target.value
+    })
+  }
+
+  renderTodoList = () => {
+    const {todoList} = this.state;
+
+    return (todoList.map((element,index) => (
+      <ToDoListItem
+        key={index}
+        element={element}
+        onClick={() => this.deleteTodo(element)}
+      />
+  )));
+  }
+
   render() {
     return (
       <div className="App">
+      <form className="App-form" onSubmit={this.handleSubmit}>
+      <label>
+        TO DO APP
+        <input
+          type="text"
+          name="title"
+          placeholder="title"
+          value={this.state.inputTitle}
+          onChange={this.handleChangeTitle}
+        />
+        <input
+        type="text"
+        name="description"
+        placeholder="description"
+        value={this.state.inputDescription}
+        onChange={this.handleChangeDescription}
+        />
+        </label>
+        <button type="submit" value="Submit">登録</button >
+      </form>
+
         <div>
-          <ToDoListItem
-          />
+          {this.renderTodoList()}
         </div>
       </div>
     );
